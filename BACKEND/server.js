4c0a417feaf18f1f8c8f68233b219f23e48411de
i-am-faizan <faizan0003khan@gmail.com>
2026-02-26 16:54:55 +0530

    update Cors

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 // Security & Standard Middlewares
 app.use(helmet()); // Security headers
-app.use(cors({ origin: 'http://localhost:5173', credentials: true })); // Enable CORS
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true })); // Enable CORS
 app.use(morgan('dev')); // Logging
 app.use(express.json()); // Parse JSON bodies
 app.use(cookieParser()); // Parse Cookies
